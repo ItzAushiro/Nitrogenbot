3283a03b772297ec8a36ec70327ac4f8e6a3a0bf
ItzAushiro <101271120+ItzAushiro@users.noreply.github.com>
2022-08-30 23:25:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,213 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const keepAlive = require('./server');
+const randomstring = require("randomstring");
+
+const prefix = 'n!';
+const amountnitro = 0;
+
+client.on('message', msg => {
+    let args = msg.content.slice(prefix.length).trim().split(' ');
+
+    if (msg.content.startsWith(`${prefix}nitro`)) {
+        msg.delete()
+        const exampleEmbed = new Discord.MessageEmbed()
+	    .setColor('RED')
+	    .setTitle('-------- Nitro --------')
+	    .setURL('https://discord.gg/2UDqWTvf53')
+	    .addFields(
+		   { name: 'DM  ', value: 'Je vous ai envoyé un 100 Nitro Vérifiez vos DM :thumbsup: (Remarque : les codes Nitro ne sont pas vérifiés)' },
+	    )
+	   .setTimestamp()
+        msg.channel.send(exampleEmbed);
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+      msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+
+msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+        msg.author.send("https://discord.gift/" + randomstring.generate(16));
+    }
+
+
+    if (msg.content.startsWith(`${prefix}chances`)) {
+        if (!msg.guild.member(client.user).hasPermission(["SEND_MESSAGES"])) return;
+        msg.delete()
+        const exampleEmbed = new Discord.MessageEmbed()
+	    .setColor('#8A2BE2')
+	    .setTitle('-------- Nitro Chances --------')
+	    .setURL('https://discord.gg/2UDqWTvf53')
+	    .addFields(
+		   { name: 'Nitro Classic : ', value: '2%' },
+		   { name: 'Nitro : ', value: '0.1%' },
+	    )
+	   .setTimestamp()
+
+        msg.channel.send(exampleEmbed);
+    }
+
+if (msg.content.startsWith(`${prefix}help`)) {
+        if (!msg.guild.member(client.user).hasPermission(["SEND_MESSAGES"])) return;
+        msg.delete()
+        const exampleEmbed = new Discord.MessageEmbed()
+	    .setColor('#8A2BE2')
+	    .setTitle('-------- Help --------')
+	    .setURL('https://discord.gg/2UDqWTvf53')
+	    .addFields(
+		   { name: 'n!helpnitro  ', value: 'Aide Commandes Nitro' },
+		   { name: 'n!nitro  ', value: 'Gen 100 Nitro à votre Dm' },
+           { name: '!chancesnitro - ', value: 'Voir les chances de Nitro' },
+	    )
+	   .setTimestamp()
+
+        msg.channel.send(exampleEmbed);
+    }
+})
+
+client.on('ready', async () => {
+    console.log('Nitro Bot V3 Is Online!')
+
+    let statuses = [
+        `NitroGen`,
+        `dsc.gg/nitrogenerator`
+    ]
+
+    setInterval(function () {
+        let status = statuses[Math.floor(Math.random() * statuses.length)];
+        client.user.setActivity(status, {
+            type: "CUSTOM_STATUS",
+            url: "to n!help"
+        })
+    }, 4000)
+})
+
+keepAlive();
+client.login(process.env.TOKEN)
